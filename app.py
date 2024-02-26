d7f4a28a75aa0eca2bc4e2923c278bce3842de7e
centristy <centristy@gmail.com>
2024-02-26 23:27:51 +0900

    Secret Key Config

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ os.environ.get('DATABASE_URL', 'postgresql://oefxxyoo:GygC5wo5vUgZLjKCXMQJMzF4hX
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 
 toolbar = DebugToolbarExtension(app)
 

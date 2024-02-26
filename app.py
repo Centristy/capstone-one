22ead64f53f3a57c3429e37e0969c11e8cabefbe
centristy <centristy@gmail.com>
2024-02-26 23:22:25 +0900

    Database URL set SQLALCHEMY

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-# app.config['SQLALCHEMY_DATABASE_URI'] = (
-# os.environ.get('DATABASE_URL', 'postgresql://oefxxyoo:GygC5wo5vUgZLjKCXMQJMzF4hXzOZFSS@stampy.db.elephantsql.com/oefxxyoo'))
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+os.environ.get('DATABASE_URL', 'postgresql://oefxxyoo:GygC5wo5vUgZLjKCXMQJMzF4hXzOZFSS@stampy.db.elephantsql.com/oefxxyoo'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
